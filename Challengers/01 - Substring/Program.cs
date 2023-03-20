eb3ac83ff93239de9ec6368ad91eb4c4735abfd3
Jean Jorge Michel <jeanjmichel@gmail.com>
2023-03-20 19:11:00 -0300

    Proposing another way to organize the repository
    Dear author, I'm proposing another way to organize the repository.

---
+++ Challengers/01 - Substring/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Challenges.Substring
+{
+    class Substring
+    {
+        private int LongestSubstring(string s, int k) { return 0; }
+
+        private void Test()
+        {
+            Console.WriteLine(new Substring().LongestSubstring("abcbdbdbbdcdabd", 2));
+        }
+    }
+}
--- Challengers/Program.cs
+++ Challengers/Program.cs
@@ -1,209 +1,40 @@
-﻿
-using Challengers;
-using System.Text.Json.Nodes;
-using System.Text.Json.Serialization;
-using Newtonsoft.Json;
-using System.Diagnostics;
-using System;
-using static System.Runtime.InteropServices.JavaScript.JSType;
-
-// create a new Stopwatch object
-Stopwatch stopwatch = new Stopwatch();
-
-
-
-
-
-
-
-
-
-
-#region Challenger 1 - Substring
-// Console.WriteLine($" Challenger 1 : {Substring01.LongestSubstring("ABCVKkkVKoiuy", 2)}");
-#endregion
-#region Challenger 2 - Max Product of 2 Integers
-// Console.WriteLine($" Challenger 2 : {MaxProductOf2Int.MaxProduct(new int[] {1,2, 10, 4, 5 })}");
-// Console.WriteLine($" Challenger 2 : {MaxProductOf2Int.MinProductOf2IntOptimized_1(new int[] { 1, 5, 3, 4, 2 })}");
-#endregion
-#region Challenger 3 - Longest Subarray
-//var target = 10;
-//var arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, -1, -5, -4}; 
-//Console.WriteLine($" Challenger 3 : {string.Join(",", LongestSubarray.FindLongestSubarrayWithSum(arr, target))}");
-//Console.WriteLine($" Challenger 3 opt : {string.Join(",", LongestSubarray.FindLongestSubarrayWithSum_optimized_1(arr, target))}");
-#endregion
-#region Challenger 4 - PasswordStrengthChecker 
-//var response1 = _04___PasswordStrengthChecker.PasswordStrengthChecker("@3$$aqAQ!@##$123****yhyhyAAAAA2545677");
-//Console.WriteLine($" Challenger 4 :{JsonConvert.SerializeObject(response1)}");
-
-//var response2 = _04___PasswordStrengthChecker.PasswordStrengthChecker("123aqA");
-//Console.WriteLine($" Challenger 4 :{JsonConvert.SerializeObject(response2)}");
-
-//Console.WriteLine($" Challenger 4 opt : { }");
-#endregion
-#region Challenger 5 - Palindrome Permutation
-////Console.WriteLine($" Challenger 5 (racecar)...: {_05____PalindromePermutation.GetPalindromePermutation("racecar")}");
-////Console.WriteLine($" Challenger 5 (Amor a Roma)...: {_05____PalindromePermutation.GetPalindromePermutation("Amor a Roma")}");
-//Console.WriteLine($" Challenger 5 (opt) (cutecute)...: {_05____PalindromePermutation.IsPalindromePermutation("cutecute")}");
-
-#endregion
-#region Challenger 6 - Vowel Count
-//Console.WriteLine($" Challenger 6 (Country)...: {_06___VowelCount.GetVowelCount("Country")}");
-
-#endregion
-#region Challenger 7 - Anagram Checker 
-//Console.WriteLine($" Challenger 7  ('testops', 'opstest')...: {AnagramChecker.AreAnagrams("testops", "opstest")}");
-//Console.WriteLine($" Challenger 7  ('dute', 'etdu')...: {AnagramChecker.AreAnagrams("dute", "etdu")}");
-//Console.WriteLine($" Challenger 7  ('this time', 'that time')...: {AnagramChecker.AreAnagrams("this time", "that time")}");
-#endregion
-#region Challenger 8 - FizzBuzz
-//FizzBuzz.ShowFizzBuzz();  
-#endregion
-#region Challenger 9 - Phone Number Formatter
-//Console.WriteLine($" Challenger 9  \"123-456-7890\"...: {PhoneNumberFormatter.GetPhoneNumberFormatter("123-456-7890")}");
-//Console.WriteLine($" Challenger 9  \"123 456 7890\"...: {PhoneNumberFormatter.GetPhoneNumberFormatter("123 456 7890")}");
-//Console.WriteLine($" Challenger 9  \"1234567890\"...: {PhoneNumberFormatter.GetPhoneNumberFormatter("1234567890")}");
-//Console.WriteLine($" Challenger 9  \"12-345-67-890\"...: {PhoneNumberFormatter.GetPhoneNumberFormatter("12-345-67-890")}");
-
-//Input: "123-456-7890"
-//Output: (123) 456 - 7890
-
-//Input: "123 456 7890"
-//Output: (123) 456 - 7890
-
-//Input: "1234567890"
-//Output: (123) 456 - 7890
-
-//Input: "12-345-67-890"
-//Output: null
-#endregion
-#region Challenger 10 - Sum of Primes
-//Console.WriteLine($" Challenger 10  ...: {SumOfPrimes.GetResult(10)}");
-#endregion
-#region Challenger 11 - Number Of Words
-// Console.WriteLine($" Challenger 11  ...: { NumberOfWords.GetNumberOfWords("This is an app to count the words!")}");
-#endregion
-#region Challenger 12 - Second Largest Number
-//Console.WriteLine($" Challenger 12  ...: {SecondLargestNumber.Get(new int[] { 1, 2, 10, 4, 5 }) }");
-#endregion
-#region Challenger 13 - Caesar Cipher (Intermediate)
-//Console.WriteLine($" Challenger 13 ...: {CaesarCipher.Get("W", 7)}");
-////    CaesarCipher.Encrypt("HELLO WORLD", 7);
-
-#endregion
-#region Challenger 14 - Binary to Decimal Converter (Easy)
-//Console.WriteLine($" Challenger 14 ...: {BinaryToDecimalConverter.Execute("1101")}");
-
-#endregion
-#region Challenger 15 - Binary to Decimal Converter (Easy)
-//Console.WriteLine($" Challenger 15 ...: {PerfectNumber.IsPerfectNumber(0)}"); // N
-//Console.WriteLine($" Challenger 15 ...: {PerfectNumber.IsPerfectNumber(1)}"); // N
-//Console.WriteLine($" Challenger 15 ...: {PerfectNumber.IsPerfectNumber(2)}"); // N
-//Console.WriteLine($" Challenger 15 ...: {PerfectNumber.IsPerfectNumber(3)}"); // N
-//Console.WriteLine($" Challenger 15 ...: {PerfectNumber.IsPerfectNumber(4)}"); // N
-//Console.WriteLine($" Challenger 15 ...: {PerfectNumber.IsPerfectNumber(5)}"); // N
-//Console.WriteLine($" Challenger 15 ...: {PerfectNumber.IsPerfectNumber(6)}"); // Y
-//Console.WriteLine($" Challenger 15 ...: {PerfectNumber.IsPerfectNumber(10)}"); // N
-//Console.WriteLine($" Challenger 15 ...: {PerfectNumber.IsPerfectNumber(28)}"); // Y
-#endregion
-#region Challenger 16 - Reverse Words (Easy)
-//var longText = "There are several ways to measure the performance of two different solutions and determine which one is better. " +
-//    "Here are some common methods: " +
-//    "1. Execution time: You can measure the time it takes for each solution to complete the task. " +
-//    "2. You can use the Stopwatch class in C# to measure the execution time of each method." +
-//    "3. Memory usage: You can measure the amount of memory each solution uses. You can use a memory profiler tool to analyze the memory " +
-//    " usage of each method." +
-//    "4. Complexity analysis: You can analyze the time and space complexity of each solution. " +
-//    " You can use Big O notation to describe the time and space complexity of each method." +
-//    "Testing: You can create a set of test cases that cover different scenarios and run them against each solution. " +
-//    "You can compare the results and determine which solution is better in terms of accuracy and correctness." +
-//    "By using one or more of these methods, you can measure the performance of different solutions and choose the one " +
-//    "that works best for your requirements.";
-
-//// Making the text big 
-//longText += longText;
-//longText += longText;
-//longText += longText;
-//longText += longText;
-//longText += longText;
-//longText += longText;
-//longText += longText;
-
-////Console.WriteLine($" Challenger 16 ...: {ReverseWords.GenerateReverseWords(longText)}");
-////Console.WriteLine($" Challenger 16 ...: {ReverseWords.ReverseStringOptmized(longText)}");
-
-//Console.WriteLine($"\nChallenger 16 | ReverseWords |");
-//stopwatch.Start();
-//ReverseWords.GenerateReverseWords(longText);
-//stopwatch.Stop();
-//Console.WriteLine($"  Memory usage...: {Process.GetCurrentProcess().PrivateMemorySize64 / 1024} Kbytes");
-//Console.WriteLine($"  Elapsed time...: {stopwatch.Elapsed.TotalMilliseconds} ms");
-//stopwatch.Reset();
-
-
-//Console.WriteLine($"\nChallenger 16 | ReverseStringOptmized |");
-//stopwatch.Start();
-//ReverseWords.ReverseStringOptmized(longText);
-//stopwatch.Stop();
-//Console.WriteLine($"  Memory usage...: {Process.GetCurrentProcess().PrivateMemorySize64 / 1024} Kbytes");
-//Console.WriteLine($"  Elapsed time...: {stopwatch.Elapsed.TotalMilliseconds} ms");
-//stopwatch.Reset();
-
-
-#endregion
-#region Challenger 17 - Armstrong Number
-//Console.WriteLine($" Challenger 17 ...: {ArmstrongNumber.IsArmstrongNumber("")}");
-//Console.WriteLine($" Challenger 17 ...: {ArmstrongNumber.IsArmstrongNumber("0")}");
-//Console.WriteLine($" Challenger 17 ...: {ArmstrongNumber.IsArmstrongNumber("-1")}");
-//Console.WriteLine($" Challenger 17 ...: {ArmstrongNumber.IsArmstrongNumber("nop")}");
-//Console.WriteLine($" Challenger 17 ...: {ArmstrongNumber.IsArmstrongNumber("153")}");
-//Console.WriteLine($" Challenger 17 ...: {ArmstrongNumber.IsArmstrongNumber("9474")}");
-//Console.WriteLine($" Challenger 17 ...: {ArmstrongNumber.IsArmstrongNumber("123")}");
-
-//Console.WriteLine($" Challenger 17 (opt)...: {ArmstrongNumber.IsArmstrongNumberOptmized(0)}");
-//Console.WriteLine($" Challenger 17 (opt)...: {ArmstrongNumber.IsArmstrongNumberOptmized(-1)}");
-//Console.WriteLine($" Challenger 17 (opt)...: {ArmstrongNumber.IsArmstrongNumberOptmized(153)}");
-//Console.WriteLine($" Challenger 17 (opt)...: {ArmstrongNumber.IsArmstrongNumberOptmized(9474)}");
-//Console.WriteLine($" Challenger 17 (opt)...: {ArmstrongNumber.IsArmstrongNumberOptmized(123)}");
-////Example:
-/////  Input: 153
-/////  Output: "Armstrong Number" 
-/////  
-/////  Input: 9474
-/////  Output: "Armstrong Number" 
-///// 
-/////  Input: 123
-/////  Output: "Not Armstrong Number"
-/////  
-
-#endregion
-#region Challenger 18 - Fibonacci (Easy)
-//Console.WriteLine($" Challenger 18 ...: {Fibonacci.GetFiboInSequence(6)}");
-#endregion
-#region Challenger 19 - Pangram Checker (Intermediate)
-//var phaseTrue = "The quick brown fox jumps over the lazy dog.";
-//var phaseFalse = "This is not a pangram.";
-
-////Console.WriteLine($" Challenger 19 ...: {Pangram.IsPangram(phaseTrue)}");
-////Console.WriteLine($" Challenger 19 ...: {Pangram.IsPangram(phaseFalse)}");
-
-//var phaseTrueOpt = "TTThe quick brown fox jumps over the lazy dog.aaaaaaaaaaa";
-//Console.WriteLine($" Challenger 19 ...: {Pangram.IsPangramOptmized(phaseTrueOpt)}");
-
-#endregion
-#region Challenger 20 - Valid Parentheses (Intermediate)
-//Console.WriteLine($" Challenger 20 ...: {ValidParentheses.IsValid("()[]") }");
-#endregion
-
-#region Challenger 21 - NextGreaterElement I (Intermediate)
-var ret = NextGreaterElementI.NextGreaterElement(new int[] { 4, 1, 2 }, new int[] { 1, 3, 4, 2 });  //nums1 = [4,1,2], nums2 = [1,3,4,2]
-Console.Write("Challenger 21...: " + string.Join(",", ret));
-#endregion
-
-#region Challenger 22 -  
- Console.Write("Challenger 22...:");
-#endregion
-
-
-
-Console.ReadKey();
+﻿namespace Challenges
+{ 
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("     ____     ___ ______         __  __ __   ____  _      _        ___  ____    ____    ___  _____");
+            Console.WriteLine("    |    \\   /  _]      |       /  ]|  |  | /    || |    | |      /  _]|    \\  /    |  /  _]/ ___/");
+            Console.WriteLine("    |  _  | /  [_|      |      /  / |  |  ||  o  || |    | |     /  [_ |  _  ||   __| /  [_(   \\_ ");
+            Console.WriteLine("    |  |  ||    _]_|  |_|     /  /  |  _  ||     || |___ | |___ |    _]|  |  ||  |  ||    _]\\__  |");
+            Console.WriteLine(" __ |  |  ||   [_  |  |      /   \\_ |  |  ||  _  ||     ||     ||   [_ |  |  ||  |_ ||   [_ /  \\ |");
+            Console.WriteLine("|  ||  |  ||     | |  |      \\     ||  |  ||  |  ||     ||     ||     ||  |  ||     ||     |\\    |");
+            Console.WriteLine("|__||__|__||_____| |__|       \\____||__|__||__|__||_____||_____||_____||__|__||___,_||_____| \\___|\n");
+            Console.WriteLine("Go to README file to understand how this works.");
+            Console.WriteLine("In this program you will be challenge to resolve more than twenty challenges, like:\n");
+            Console.WriteLine("Challenge 01: Find the length of the longest substring that contains at most k distinct characters");
+            Console.WriteLine("Challenge 02: Find the maximum product of any two distinct elements in the array");
+            Console.WriteLine("Challenge 03: Find the longest subarray with a sum that equals a given target value");
+            Console.WriteLine("Challenge 04: Returns a score indicating the strength of the password");
+            Console.WriteLine("Challenge 05: Palindrome Permutation");
+            Console.WriteLine("Challenge 06: Vowel Count");
+            Console.WriteLine("Challenge 07: Anagram Checker");
+            Console.WriteLine("Challenge 08: FizzBuzz");
+            Console.WriteLine("Challenge 09: Phone Number Formatter");
+            Console.WriteLine("Challenge 10: Sum of Primes");
+            Console.WriteLine("Challenge 11: Number of words");
+            Console.WriteLine("Challenge 12: Second Largest Number");
+            Console.WriteLine("Challenge 13: Caesar Cipher");
+            Console.WriteLine("Challenge 14: Binary to Decimal Converter");
+            Console.WriteLine("Challenge 15: Perfect Number");
+            Console.WriteLine("Challenge 16: Reverse Words");
+            Console.WriteLine("Challenge 17: Armstrong Number");
+            Console.WriteLine("Challenge 18: Fibonacci Sequence");
+            Console.WriteLine("Challenge 19: Pangram Checker");
+            Console.WriteLine("Challenge 20: Valid Parentheses");
+            Console.WriteLine("Challenge 21: Next Greater Element I");
+            Console.ReadKey();
+        }
+    }
+}
